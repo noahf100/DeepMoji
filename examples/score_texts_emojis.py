ca9b3b4d2fbb656bb763e9c21ae6bbb052dc9451
Noah Fang <noahf10@gmail.com>
2021-08-29 01:01:37 -0400

    Maybe fix for module

--- examples/score_texts_emojis.py
+++ examples/score_texts_emojis.py
@@ -12,9 +12,9 @@ import example_helper
 import json
 import csv
 import numpy as np
-from deepmoji.sentence_tokenizer import SentenceTokenizer
-from deepmoji.model_def import deepmoji_emojis
-from deepmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
+from ..deepmoji.sentence_tokenizer import SentenceTokenizer
+from ..deepmoji.model_def import deepmoji_emojis
+from ..deepmoji.global_variables import PRETRAINED_PATH, VOCAB_PATH
 
 OUTPUT_PATH = 'test_sentences.csv'
 
